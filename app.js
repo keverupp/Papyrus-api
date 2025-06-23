af0a5208b6d181db82681d4ecc93b7b0560a3acc
PolixTer <clevertonruppenthal1@gmail.com>
2025-06-23 11:31:52 -0300

    Ajustes para rodar no windows

--- app.js
+++ app.js
@@ -1,28 +1,36 @@
-'use strict'
+"use strict";
 
-const path = require('path')
-const autoload = require('@fastify/autoload')
+const path = require("path");
+const autoload = require("@fastify/autoload");
 
-const fastify = require('fastify')
+const fastify = require("fastify");
 
 const app = fastify({
-  logger: false
-})
+  logger: false,
+});
 
 app.register(autoload, {
-  dir: path.join(__dirname, 'src/plugins')
-})
+  dir: path.join(__dirname, "src/plugins"),
+});
 
 app.register(autoload, {
-  dir: path.join(__dirname, 'src/hooks')
-})
+  dir: path.join(__dirname, "src/hooks"),
+});
 
 app.register(autoload, {
-  dir: path.join(__dirname, 'src/services')
-})
+  dir: path.join(__dirname, "src/services"),
+});
 
 app.register(autoload, {
-  dir: path.join(__dirname, 'src/routes')
-})
+  dir: path.join(__dirname, "src/routes"),
+});
 
-app.listen({ host: '0.0.0.0', port: 4000 })
+app
+  .listen({ host: "0.0.0.0", port: 4000 })
+  .then((address) => {
+    console.log(`🚀 Servidor rodando em: ${address}`);
+  })
+  .catch((err) => {
+    console.error("❌ Erro ao iniciar o servidor:", err);
+    process.exit(1);
+  });
