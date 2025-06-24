abb408f4ba878eafc2f2922a22c808d6fa6772aa
PolixTer <clevertonruppenthal1@gmail.com>
2025-06-24 16:11:22 -0300

    Remoções não necessarias do boilerplate

--- src/plugins/multipart.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-const fp = require('fastify-plugin')
-
-module.exports = fp(async (app) => {
-  app.register(require('@fastify/multipart'))
-})
--- src/routes/socket/index.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-module.exports = async (app) => {
-
-  app.post('/', async (request, reply) => {
-    app.io.emit('chat:0001', request.body.message)
-    return reply.send('Mensagem enviada')
-  })
-}
